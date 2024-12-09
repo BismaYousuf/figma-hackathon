72bb345e333efdf8f668337784598d7a24d69ff7
BismaYousuf <bismayousuf540@gmail.com>
2024-12-09 12:37:03 -0600

    up5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-   <div className="min-h-screen bg-black">
+   <div>
     <Hero/>
    <AboutUs/>
    <FoodCategory/>
